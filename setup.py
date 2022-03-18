82c63149b1718deee3a28ae8bb5b81ed8032df3f
zhouyuansheng0618 <49557312+zhouyuansheng0618@users.noreply.github.com>
2022-03-18 14:59:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,10 @@ from distutils.core import setup
 setup(name='st2-auth-backend-adfs',
       version='1.0',
       py_modules=['st2-auth-backend-adfs'],
-      zip_safe=False)
+      zip_safe=False,
+      entry_points={
+            'st2auth.sso.backends': [
+                  'saml2 = st2auth_sso_adfs.adfs_backend:ADFSAuthenticationBackend'
+            ]
+      }
+      )
